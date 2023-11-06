c9c3538e8fc7e1dbafeff8b18d26390d4ac00caa
Romeo Valentin <romeov@stanford.edu>
2023-11-06 13:56:06 -0800

    Add tests for matching pursuit
    Looking good! :)

--- test/matching_pursuit.jl
+++ test/matching_pursuit.jl
@@ -1,47 +1,79 @@
-D = [
-	-5  -9  -9   7   0;
-	 7   0   4   0   9;
-	 8   0   5   4  -6
-]
+import Random: seed!, TaskLocalRNG
 
 
-# when data is representad as a vector
-Y = [
-    -1;
-     0;
-     5
-]
+@testset "Compare Matching Pursuit implementations" begin
+    rng = TaskLocalRNG();
+    seed!(rng, 1)
 
-X = matching_pursuit(Y, D)
-@test norm(Y-D*X) < 1e-6
+    T = Float64
+    D, N = 100, 1000
+    data = rand(rng, D, N)
+    B = KSVD.init_dictionary(T, D, 2*D)
+
+    res_baseline = KSVD.sparse_coding(KSVD.LegacyMatchingPursuit(), data, B)
+
+    if Threads.nthreads == 1
+        @warn "Parallel implementation will only be tested properly if test is launched with multiple threads!"
+    end
+    @testset for method in [KSVD.MatchingPursuit(),
+                            KSVD.ParallelMatchingPursuit(),
+                            KSVD.FasterParallelMatchingPursuit(),
+                            KSVD.CUDAAcceleratedMatchingPursuit(batch_size=300) # batch_size<N and batch_size÷N != 0 for test
+                            ]
+
+        res = KSVD.sparse_coding(method, data, B)
+        @test res ≈ res_baseline
+    end
+    @testset for method in [KSVD.FullBatchMatchingPursuit,]
+        res = KSVD.sparse_coding(method(), data, B)
+        @test res ≈ res_baseline broken=true
+    end
+end
+
+# D = [
+# 	-5  -9  -9   7   0;
+# 	 7   0   4   0   9;
+# 	 8   0   5   4  -6
+# ]
 
 
-# when data is representad as a matrix
-Y = [
-    -1  2;
-     0  3;
-     5 -5
-]
+# # when data is representad as a vector
+# Y = [
+#     -1;
+#      0;
+#      5
+# ]
 
-X = matching_pursuit(Y, D)
-@test norm(Y-D*X) < 1e-6
-
-
-# when max_iter < 0
-@test_throws ArgumentError matching_pursuit(Y, D; max_iter = 0)
-@test_throws ArgumentError matching_pursuit(Y, D; max_iter = -1)
+# X = matching_pursuit(Y, D)
+# @test norm(Y-D*X) < 1e-6
 
 
-# when tolerance < 0
-@test_throws ArgumentError matching_pursuit(Y, D, tolerance = 0.)
-@test_throws ArgumentError matching_pursuit(Y, D; tolerance = -1.0)
+# # when data is representad as a matrix
+# Y = [
+#     -1  2;
+#      0  3;
+#      5 -5
+# ]
+
+# X = matching_pursuit(Y, D)
+# @test norm(Y-D*X) < 1e-6
 
 
-# when dimensions of data and atoms do not match
+# # when max_iter < 0
+# @test_throws ArgumentError matching_pursuit(Y, D; max_iter = 0)
+# @test_throws ArgumentError matching_pursuit(Y, D; max_iter = -1)
 
-Y = [
-     2;
-    -5
-]
 
-@test_throws ArgumentError matching_pursuit(Y, D)
+# # when tolerance < 0
+# @test_throws ArgumentError matching_pursuit(Y, D, tolerance = 0.)
+# @test_throws ArgumentError matching_pursuit(Y, D; tolerance = -1.0)
+
+
+# # when dimensions of data and atoms do not match
+
+# Y = [
+#      2;
+#     -5
+# ]
+
+# @test_throws ArgumentError matching_pursuit(Y, D)
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,7 @@
 using KSVD
 using LinearAlgebra
+# using ReTest
 using Test
 
-include("ksvd.jl")
+# include("ksvd.jl")
 include("matching_pursuit.jl")
