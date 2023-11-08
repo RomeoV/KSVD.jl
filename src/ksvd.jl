154ccddb8fdeea3260e376b71cb3f41acf87eccf
Romeo Valentin <romeov@stanford.edu>
2023-11-08 15:33:08 -0800

    Remove old ParallelKSVD implementation

--- src/ksvd.jl
+++ src/ksvd.jl
@@ -48,71 +48,6 @@ function ksvd(method::LegacyKSVD, Y::AbstractMatrix, D::AbstractMatrix, X::Abstr
     return D, X
 end
 
-function ksvd2(method::ParallelKSVD, Y::AbstractMatrix{T}, D::AbstractMatrix{T}, X::AbstractMatrix{T}; err_buffers=nothing, err_gamma_buffers=nothing) where T
-    N = size(Y, 2)
-    # Eₖ = Y .- D * X
-    D_t = Matrix(transpose(D));
-    Eₖ = transpose(.-X'*D_t .+ Y')
-    X_cpy = copy(X)
-    D_cpy = copy(D)
-
-    # preallocate error buffers
-    Eₖ_buffers = (!isnothing(err_buffers) ? tcollect(copyto!(buf, Eₖ) for buf in  err_buffers) :
-                  (method.prealloc_buffers ? [copy(Eₖ) for _ in 1:Threads.nthreads()] :
-                   nothing))
-    E_Ω_buffers = (!isnothing(err_buffers) ? err_gamma_buffers :
-                  (method.prealloc_buffers ? [similar(Eₖ) for _ in 1:Threads.nthreads()] :
-                   nothing))
-
-    # Note: we need :static to use threadid, see https://julialang.org/blog/2023/07/PSA-dont-use-threadid/
-    basis_indices = (method.shuffle_indices ? shuffle(axes(X, 1)) : axes(X, 1))
-    Threads.@threads :static for k in basis_indices
-        xₖ = X[k, :]
-        # ignore if the k-th row is zeros
-        all(iszero, xₖ) && continue
-
-        # wₖ is the column indices where the k-th row of xₖ is non-zero,
-        # which is equivalent to [i for i in N if xₖ[i] != 0]
-        ωₖ = findall(!iszero, xₖ)
-        Ωₖ = sparse(ωₖ, 1:length(ωₖ), ones(T, length(ωₖ)), N, length(ωₖ))
-
-        # Eₖ * Ωₖ implies a selection of error columns that
-        # correspond to examples that use the atom D[:, k]
-        Eₖ_local = (!isnothing(Eₖ_buffers) ? Eₖ_buffers[Threads.threadid()] : copy(Eₖ))
-        # Eₖ .+= D[:, k:k] * X[k:k, :]
-        for (j, X_val) in zip(findnz(X[k, :])...)
-            @inbounds @views Eₖ_local[:, j] .+=  X_val .* D[:, k]  # this compiles to something similar to axpy!, i.e. no allocations. Notice we need the dot also for the scalar mul.
-        end
-
-        # Note that S is a vector that contains diagonal elements of
-        # a matrix Δ such that Eₖ * Ωₖ == U * Δ * V.
-        # Non-zero entries of X are set to
-        # the first column of V multiplied by Δ(1, 1)
-        # E_Ω = Eₖ_local * Ωₖ
-        E_Ω = (!isnothing(E_Ω_buffers) ? let
-                    buf = E_Ω_buffers[Threads.threadid()]
-                    @inbounds @view buf[:, 1:length(ωₖ)]
-               end : similar(Eₖ, size(Eₖ, 1), length(ωₖ)))
-        for (i, j, Ω_val) in zip(findnz(Ωₖ)...)
-            @inbounds @views E_Ω[:, j] .=  Ω_val .* Eₖ_local[:, i]  # this compiles to something similar to axpy!, i.e. no allocations. Notice we need the dot also for the scalar mul.
-        end
-        U, S, V = if size(E_Ω, 2) <= 5
-            svd!(E_Ω)
-        else
-            # tsvd(E_Ω)                 # second hotspot
-            svd!(E_Ω)
-        end
-        @inbounds @views D_cpy[:, k] .= U[:, 1]
-        @inbounds @views X_cpy[k, ωₖ] .= S[1] .* V[:, 1]
-        # reverse the local error matrix to be reused without copy
-        # Eₖ .-= D[:, k:k] * X[k:k, :]
-        for (j, X_val) in zip(findnz(X[k, :])...)
-            @inbounds @views Eₖ_local[:, j] .-=  X_val .* D[:, k]  # this compiles to something similar to axpy!, i.e. no allocations. Notice we need the dot also for the scalar mul.
-        end
-    end
-    return D_cpy, X_cpy
-end
-
 function ksvd(method::ParallelKSVD, Y::AbstractMatrix{T}, D::AbstractMatrix{T}, X::AbstractMatrix{T}; svd_method=svd!) where T
     N = size(Y, 2)
     # E .= Y - D*X
@@ -170,23 +105,20 @@ function ksvd(method::ParallelKSVD, Y::AbstractMatrix{T}, D::AbstractMatrix{T}, 
 
         @inbounds @views E_Ω[:, 1:length(ωₖ)] .= E[:, ωₖ]
         @inbounds @views for (j_dest, j) in enumerate(ωₖ)
+            # this is insanely slow...
             # E_Ω[:, j_dest] .+= D * X[:, j]
             # See https://stackoverflow.com/a/52615073/5616591
             # This seems to be faster than `findnz` because it doesn't allocate at all.
-            rs = nzrange(X, j)
-            # E_Ω[:, j_dest] .+=  sum(D[:, rowvals(X)[rs]] * nonzeros(X)[rs], dims=2)
-            for (k, X_val) in zip(rowvals(X)[rs], nonzeros(X)[rs])
-                E_Ω[:, j_dest] .+=  X_val .* D[:, k]
-            end
+            # These next two are pretty competitive.
+            @fastmath E_Ω[:, j_dest] .+=  D[:, rowvals(X)[rs]] * nonzeros(X)[rs]
+            # rs = nzrange(X, j)
+            # @fastmath for (k, X_val) in zip(rowvals(X)[rs], nonzeros(X)[rs])
+            #     E_Ω[:, j_dest] .+=  X_val .* D[:, k]
+            # end
             # for (X_val, k) in findnz(X[:, j])
             #     E_Ω[:, j_dest] .+=  X_val .* D[:, k]  # this compiles to something similar to axpy!, i.e. no allocations. Notice we need the dot also for the scalar mul.
             # end
         end
-        # for i in axes(X, 1)
-        #     for (j, X_val) in (@inbounds @views zip(findnz(X[i, ωₖ])...))
-        #         @inbounds @views E_Ω[:, j] .+=  X_val .* D[:, i]  # this compiles to something similar to axpy!, i.e. no allocations. Notice we need the dot also for the scalar mul.
-        #     end
-        # end
         for (j, X_val) in zip(findnz(X[k, ωₖ])...)
             @inbounds @views E_Ω[:, j] .+=  X_val .* D[:, k]  # this compiles to something similar to axpy!, i.e. no allocations. Notice we need the dot also for the scalar mul.
         end
