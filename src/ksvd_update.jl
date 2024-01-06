2446a09d260d834eccc38a28cbb0a46b07e49ea4
Romeo Valentin <romeov@stanford.edu>
2024-01-06 12:56:44 -0800

    Added a bunch of comments, which magically fixed the tests (?!)

--- src/ksvd_update.jl
+++ src/ksvd_update.jl
@@ -97,11 +97,15 @@ sparsecsr(M_t::Adjoint{SparseMatrixCSC}) = sparsecsr(findnz(parent(M_t))[[2,1,3]
                 @view buf[:, 1:length(ωₖ)]
             end
 
-            # make sure not to use `@view` on `X`, see https://github.com/JuliaSparse/SparseArrays.jl/issues/475
+            ##<BEGIN OPTIMIZED BLOCK>
+            ## Original:
             # E_Ω .= Y[:, ωₖ] - D * X[:, ωₖ] + D[:, k] * X[k, ωₖ]'
+            ## Faster:
             E_Ω .= @view Y[:, ωₖ]
+            # Note: Make sure not to use `@view` on `X`, see https://github.com/JuliaSparse/SparseArrays.jl/issues/475
             fastdensesparsemul!(E_Ω, D, X[:, ωₖ], -1, 1)
             fastdensesparsemul_outer!(E_Ω, @view(D[:, k]), X[k, ωₖ], 1, 1)
+            ## <END OPTIMIZED BLOCK>
 
             # truncated svd has some problems for column matrices. so then we just do svd.
             U, S, V = (size(E_Ω, 2) <= 3 ? svd!(E_Ω) : tsvd(E_Ω, 1; tolconv=100*eps(eltype(E_Ω))))
@@ -114,6 +118,10 @@ sparsecsr(M_t::Adjoint{SparseMatrixCSC}) = sparsecsr(findnz(parent(M_t))[[2,1,3]
         # we can exploit that the new nonzero indices don't change!
         # Note: This doesn't seem to help in the sparse copy above.
         row_indices = SparseArrays.rowvals(X) .∈ [index_batch]
+        # Note: The next two lines are technically unnecessary, but sometimes the tests fail without them...
+        # Maybe weirdly connected to a race condition?
+        # row_indices_cpy = SparseArrays.rowvals(X_cpy) .∈ [index_batch]
+        # @assert row_indices == row_indices_cpy
         nzvalview(X)[row_indices] .= nzvalview(X_cpy)[row_indices]
 
     end
@@ -163,11 +171,19 @@ end
         fastdensesparsemul_threaded!(E, @view(D[:, index_batch]), X[index_batch, :], 1, 1)
         fastdensesparsemul_threaded!(E, @view(D_cpy[:, index_batch]), X_cpy[index_batch, :], -1, 1)
         D[:, index_batch] .= D_cpy[:, index_batch]
+        # # <BEGIN OPTIMIZED BLOCK>.
+        # # Original:
         # X[index_batch, :] .= X_cpy[index_batch, :]
+        # # Optimized:
         # we can exploit that the new nonzero indices don't change!
         # Note: This doesn't seem to help in the sparse copy above.
         row_indices = SparseArrays.rowvals(X) .∈ [index_batch]
+        # Note: The next two lines are technically unnecessary, but sometimes the tests fail without them...
+        # Maybe weirdly connected to a race condition?
+        #row_indices_cpy = SparseArrays.rowvals(X_cpy) .∈ [index_batch]
+        #@assert row_indices == row_indices_cpy
         nzvalview(X)[row_indices] .= nzvalview(X_cpy)[row_indices]
+        # # <END OPTIMIZED BLOCK>
     end
     return D, X
 end
