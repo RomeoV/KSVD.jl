c780c9f4219e9341656097b567b2b5c618ca3a9f
Romeo Valentin <romeov@stanford.edu>
2025-04-13 16:17:57 -0700

    Implement ArnoldiSVDSolver

--- src/svd_solvers.jl
+++ src/svd_solvers.jl
@@ -2,6 +2,10 @@ using KrylovKit
 import TSVD: tsvd
 import Arpack: svds
 # import ArnoldiMethod: arnoldi_svd
+using ArnoldiMethod
+
+import LinearAlgebra
+using LinearAlgebra: mul!, eltype, size
 
 abstract type AbstractTruncatedSVD end
 
@@ -10,7 +14,7 @@ abstract type AbstractTruncatedSVD end
     maxiter::Int = 50
 end
 
-@kwdef struct ArpackSolver{T<:Number} <: AbstractTruncatedSVD
+@kwdef struct ArpackSVDSolver{T<:Number} <: AbstractTruncatedSVD
     tolconv::Float64 = sqrt(eps(real(T)))
     maxiter::Int = 50
 end
@@ -30,7 +34,7 @@ function compute_truncated_svd(solver::TSVDSolver, A::AbstractMatrix{T}, k::Int)
     tsvd(A, k; tolconv=solver.tolconv, maxiter=solver.maxiter)
 end
 
-function compute_truncated_svd(solver::ArpackSolver, A::AbstractMatrix{T}, k::Int) where {T}
+function compute_truncated_svd(solver::ArpackSVDSolver, A::AbstractMatrix{T}, k::Int) where {T}
     # we copy here because we've had issues with @view and Arpack before...
     (U, S, V), _ = svds(copy(A); nsv=k, maxiter=solver.maxiter, tol=solver.tolconv)
     (U, S, V)
@@ -40,11 +44,44 @@ function compute_truncated_svd(solver::KrylovSVDSolver, A::AbstractMatrix{T}, k:
     krylov_svd(A, k; tol=solver.tol)
 end
 
-function compute_truncated_svd(solver::ArnoldiSVDSolver, A::AbstractMatrix{T}, k::Int) where {T}
-    arnoldi_svd(A, k; tol=solver.tol)
-end
-
 function krylov_svd(A, nev=1; tol=1e-8)
     S, U, V, info = svdsolve(A, nev; tol)
     stack(U; dims=2), S, stack(V, dims=2)
+end
+
+
+"To compute svd(A) = eigen(A*A'), but without doing the multiplication."
+struct LazySym{A_t,V_t,T}
+    A::A_t
+    z::V_t
+    LazySym(A) = new{typeof(A),Vector{eltype(A)},eltype(A)}(
+        A, zeros(eltype(A), size(A, 2))
+    )
+end
+Base.size(S::LazySym) = (size(S.A, 1), size(S.A, 1))
+Base.size(S::LazySym, i) = size(S)[i]
+Base.eltype(S::LazySym) = eltype(S.A)
+"Computes A * A', lazily."
+LinearAlgebra.mul!(y, S::LazySym, x) =
+    let
+        mul!(S.z, S.A', x)
+        mul!(y, S.A, S.z)
+    end
+
+function compute_truncated_svd(solver::ArnoldiSVDSolver, A::AbstractMatrix{T}, k::Int) where {T}
+    m, n = size(A)
+    (U, S, V) = if m > n  # Tall matrix, decompose A^T * A
+        (; Q, R, eigenvalues), _ = partialschur(A' * A; nev=k, tol=solver.tol)
+        V = Q
+        Sigma = sqrt.(real.(eigenvalues))
+        U = A * V * Diagonal(1 ./ Sigma)
+        (U, Sigma, V)
+    else  # Wide matrix, decompose A * A^T
+        (; Q, R, eigenvalues), _ = partialschur(A * A'; nev=k, tol=solver.tol)
+        U = Q
+        Sigma = sqrt.(real.(eigenvalues))
+        V = A' * U * Diagonal(1 ./ Sigma)
+        (U, Sigma, V)
+    end
+    return (U, S, V)
 end
