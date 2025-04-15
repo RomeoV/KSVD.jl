c15c04a00fbbabb09de1538b143b8108e3104713
Romeo Valentin <romeov@stanford.edu>
2025-04-15 00:46:17 -0700

    Fix krylov svd

--- src/svd_solvers.jl
+++ src/svd_solvers.jl
@@ -46,7 +46,7 @@ end
 
 function krylov_svd(A, nev=1; tol=1e-8)
     S, U, V, info = svdsolve(A, nev; tol)
-    stack(U; dims=2), S, stack(V, dims=2)
+    stack(U; dims=2)[:, 1:nev], S[1:nev], stack(V, dims=2)[:, 1:nev]
 end
 
 
