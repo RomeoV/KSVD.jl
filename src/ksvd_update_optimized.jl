885edc0d6af4c86104fc9c7ca08956f2cb5f1abe
Romeo Valentin <romeov@stanford.edu>
2025-04-21 17:40:41 -0700

    Rewrite optimized ksvd

--- src/ksvd_update_optimized.jl
+++ src/ksvd_update_optimized.jl
@@ -2,41 +2,46 @@ import SparseArrays: nonzeroinds
 
 @inbounds function ksvd_update(method::OptimizedKSVD, Y::AbstractMatrix{T}, D::AbstractMatrix{T}, X::AbstractMatrix{T}; timer=TimerOutput()) where {T}
     @timeit_debug timer "KSVD update" begin
+        # Eₖ = Y - D * X
+        E_buffer = similar(Y) .* false  # set to 0. Note that (.*0) doesn't work with NaN.
+        basis_indices = (method.shuffle_indices ? shuffle(axes(X, 1)) : axes(X, 1))
+        for k in basis_indices
+            @timeit_debug timer "find nonzeros" begin
+                xₖ = X[k, :]
+                # ignore if the k-th row is zeros
+                all(iszero, xₖ) && continue
+            end
 
-    Eₖ = Y - D * X
-    E_Ω_buffer = similar(Eₖ) .* false  # set to 0. Note that (.*0) doesn't work with NaN.
-    basis_indices = (method.shuffle_indices ? shuffle(axes(X, 1)) : axes(X, 1))
-    for k in basis_indices
-        xₖ = X[k, :]
-        # ignore if the k-th row is zeros
-        all(iszero, xₖ) && continue
+            # wₖ is the column indices where the k-th row of xₖ is non-zero,
+            # which is equivalent to [i for i in N if xₖ[i] != 0]
+            ωₖ = nonzeroinds(xₖ)
+            @timeit_debug timer "allocate buffer" begin
+                E_Ω = @view E_buffer[:, 1:length(ωₖ)]
+            end
 
-        # wₖ is the column indices where the k-th row of xₖ is non-zero,
-        # which is equivalent to [i for i in N if xₖ[i] != 0]
-        ωₖ = nonzeroinds(xₖ)
+            @timeit_debug timer "do math" begin
+                @timeit_debug timer "copy Y" begin
+                    E_Ω .= @view Y[:, ωₖ]
+                end
+                @timeit_debug timer "E -= D X" begin
+                    # E_Ω .-= D * X[:, ωₖ]
+                    fastdensesparsemul_threaded!(E_Ω, D, X[:, ωₖ], -1, 1)
+                end
+                @timeit_debug timer "E += D_k X_k" begin
+                    # E_Ω .+= @view(D[:, k]) * nonzeros(xₖ)'
+                    fastdensesparsemul_outer_threaded!(E_Ω, @view(D[:, k]), xₖ[ωₖ], true, true)
+                end
+            end
 
-        # Eₖ * Ωₖ implies a selection of error columns that
-        # correspond to examples that use the atom D[:, k]
-        Eₖ .+= D[:, k] * X[k, :]'
-
-        # Ωₖ = sparse(ωₖ, 1:length(ωₖ), ones(length(ωₖ)), N, length(ωₖ))
-
-        # Note that S is a vector that contains diagonal elements of
-        # a matrix Δ such that Eₖ * Ωₖ == U * Δ * V.
-        # Non-zero entries of X are set to
-        # the first column of V multiplied by Δ(1, 1)
-        # U, S, V = tsvd(Eₖ * Ωₖ, initvec=randn!(similar(Eₖ, size(Eₖ,1))))
-        # E_Ω = Eₖ * Ωₖ
-        E_Ω = @view E_Ω_buffer[:, 1:length(ωₖ)]
-        E_Ω .= Eₖ[:, ωₖ]
-
-        # U, S, V = (size(E_Ω, 2) < 3 ? svd!(E_Ω) : tsvd(E_Ω, 1; tolconv=10*eps(eltype(E_Ω))) )
-        U, S, V = (size(E_Ω, 2) <= 3 ? svd!(E_Ω) : krylov_svd(E_Ω, 1; tol=1e-10))
-        D[:, k]  .=  sign(U[1,1])       .* @view(U[:, 1])
-        X[k, ωₖ] .= (sign(U[1,1])*S[1]) .* @view(V[:, 1])
-
-        Eₖ .-= D[:, k:k] * X[k:k, :]
-    end
+            @timeit_debug timer "svd step" begin
+                U, S, V = (size(E_Ω, 2) < 3 ? svd!(E_Ω) : compute_truncated_svd(method.svd_solver, E_Ω, 1))
+                # U, S, V = (size(E_Ω, 2) <= 3 ? svd!(E_Ω) : krylov_svd(E_Ω, 1; tol=1e-10))
+            end
+            @timeit_debug timer "update D and X" begin
+                D[:, k] .= sign(U[1, 1]) .* @view(U[:, 1])
+                X[k, ωₖ] .= (sign(U[1, 1]) * S[1]) .* @view(V[:, 1])
+            end
+        end
 
     end  # @timeit_debug
     return D, X
