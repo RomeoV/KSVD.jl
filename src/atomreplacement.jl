17f48b518a8d8a7f5c798979cd154ec39f1fedfa
Romeo Valentin <romeov@stanford.edu>
2025-04-23 23:01:10 -0700

    Add `fasterror` shortcut function

--- src/atomreplacement.jl
+++ src/atomreplacement.jl
@@ -1,3 +1,16 @@
+"""
+    fasterror(Y, D, X; timer::TimerOutput=TimerOutput())
+
+See `fasterror!`.
+"""
+fasterror(Y, D, X; timer::TimerOutput=TimerOutput()) = fasterror!(similar(Y), Y, D, X; timer)
+
+"""
+    fasterror!(E, Y, D, X; timer::TimerOutput=TimerOutput())
+
+Computes the error `E = Y - D * X`. This function is optimized to only compute the
+columns of E for which the corresponding column of X is non-zero.
+"""
 function fasterror!(E, Y, D, X; timer::TimerOutput=TimerOutput())
     @timeit_debug timer "compute fast error" begin
         E .= copy(Y)
