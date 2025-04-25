b9a46d8858b3fe72b86d27dca7d676c61835b8a1
Romeo Valentin <romeov@stanford.edu>
2025-04-25 14:39:03 -0700

    Rename tolconv -> tol for interface unification

--- src/svd_solvers.jl
+++ src/svd_solvers.jl
@@ -10,33 +10,33 @@ using LinearAlgebra: mul!, eltype, size
 abstract type AbstractTruncatedSVD end
 
 @kwdef struct TSVDSolver{T<:Number} <: AbstractTruncatedSVD
-    tolconv::Float64 = 100*(eps(real(T)))
+    tol::Float64 = 100 * (eps(real(T)))
     maxiter::Int = 50
 end
 
 @kwdef struct ArpackSVDSolver{T<:Number} <: AbstractTruncatedSVD
-    tolconv::Float64 = 100*(eps(real(T)))
+    tol::Float64 = 100 * (eps(real(T)))
     maxiter::Int = 50
 end
 
 @kwdef struct KrylovSVDSolver{T<:Number} <: AbstractTruncatedSVD
-    tol::Float64 = 100*(eps(real(T)))
+    tol::Float64 = 100 * (eps(real(T)))
     maxiter::Int = 50
 end
 
 @kwdef struct ArnoldiSVDSolver{T<:Number} <: AbstractTruncatedSVD
-    tol::Float64 = 100*(eps(real(T)))
+    tol::Float64 = 100 * (eps(real(T)))
     maxiter::Int = 50
 end
 
 # Solver implementations
 function compute_truncated_svd(solver::TSVDSolver, A::AbstractMatrix{T}, k::Int) where {T}
-    tsvd(A, k; tolconv=solver.tolconv, maxiter=solver.maxiter)
+    tsvd(A, k; tolconv=solver.tol, maxiter=solver.maxiter)
 end
 
 function compute_truncated_svd(solver::ArpackSVDSolver, A::AbstractMatrix{T}, k::Int) where {T}
     # we copy here because we've had issues with @view and Arpack before...
-    (U, S, V), _ = svds(copy(A); nsv=k, maxiter=solver.maxiter, tol=solver.tolconv)
+    (U, S, V), _ = svds(copy(A); nsv=k, maxiter=solver.maxiter, tol=solver.tol)
     (U, S, V)
 end
 
