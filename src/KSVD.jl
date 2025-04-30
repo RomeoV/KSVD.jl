3f22a4c2f797afc9219609d0f3b083a147dc06f3
Romeo Valentin <romeov@stanford.edu>
2025-04-30 00:21:17 -0700

    fixup! Implement `fasterror!`

--- src/KSVD.jl
+++ src/KSVD.jl
@@ -34,6 +34,7 @@ include("ksvd_update.jl")
 include("ksvd_update_legacy.jl")
 include("ksvd_update_optimized.jl")
 include("ksvd_update_threaded_utils.jl")
+include("atomreplacement.jl")
 function __init__()
     set_num_threads(Threads.nthreads())
 end
