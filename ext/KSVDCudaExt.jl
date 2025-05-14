dc03eee6f674e1c283d693d0416f407951f0cdbe
Romeo Valentin <romeov@stanford.edu>
2025-05-13 18:16:27 -0700

    Simplify CUDA Sparse Coding
    Kind of slow now. Maybe some of the previous optimzations were a good idea.

--- ext/KSVDCudaExt.jl
+++ ext/KSVDCudaExt.jl
@@ -3,7 +3,7 @@ using CUDA
 import KSVD
 import KSVD: sparse_coding, czip
 using SparseArrays: sparse, nonzeroinds, nonzeros
-include("batched_device_op.jl")
+# include("batched_device_op.jl")
 
 """ CUDA Accelerated version pipelining the dictionary * data computation
 on the gpu, and using the result on the cpu. Always performs batching, i.e. uses
@@ -15,37 +15,54 @@ batches of data with size number of samples `batch_size` are loaded, moved to th
 and the result is moved back to the cpu. This happens asynchronously, so that the memory movement
 CPU->GPU and GPU->CPU, aswell as the computation on the CPU, are pipelined using Julia's `Channel`s.
 """
-@kwdef mutable struct CUDAAcceleratedMatchingPursuit <: KSVD.GPUAcceleratedMatchingPursuit
-    max_nnz::Int = KSVD.default_max_nnz
-    max_iter::Int = 4*max_nnz
-    rtol = KSVD.default_rtol
-    batch_size::Int = 1_000
-    workspace::Union{BatchedDeviceOpWorkspace{Float32}, Nothing} = nothing
+@kwdef struct CUDAAcceleratedMatchingPursuit <: KSVD.GPUAcceleratedMatchingPursuit
+    max_nnz::Int = default_max_nnz
+    max_iter::Int = 4 * max_nnz
+    rtol = default_rtol
+    precompute_products = true
     CUDAAcceleratedMatchingPursuit(args...) = (KSVD.validate_mp_args(args...); new(args...))
 end
 
 function KSVD.sparse_coding(method::CUDAAcceleratedMatchingPursuit,
-                            data::AbstractMatrix{T}, dictionary::AbstractMatrix{T}) where {T}
-    K = size(dictionary, 2)
-    N = size(data, 2)
+    data::AbstractMatrix{T}, dictionary::AbstractMatrix{T};
+    DtD=let Ddev = cu(dictionary)
+        Matrix(Ddev' * Ddev)
+    end,
+    Ydev=cu(data),
+    DtY=let Ddev = cu(dictionary)
+        Matrix(Ddev' * Ydev)
+    end) where {T}
 
-    DtD = dictionary'*dictionary
-    Dt_dev = CuMatrix(dictionary')
-
-    op(Y_batch::CuMatrix) = Dt_dev * Y_batch
-    if isnothing(method.workspace)
-        method.workspace = make_batched_device_op_workspace(op, data)
-    end
-    # workspace = BatchedDeviceOpWorkspace(4096, 2*512, 2*512)
-    # ch, task = batched_device_op(op, workspace, Y)
-    ch, task = batched_device_op(op, method.workspace, data)
-
-    X_batches = map(ch) do (idx, DtY)
-        idx=>sparse_coding(ParallelMatchingPursuit(), @view(data[:, idx]), dictionary; DtY, DtD)
-    end
-    # X_batches = collect(ch)
-    # @info size(X_batches), fetch(task)
-    hcat(last.(sort(collect(X_batches); by=first))...)
+    noncudamethod = KSVD.ParallelMatchingPursuit(;
+        max_nnz=method.max_nnz,
+        max_iter=method.max_iter,
+        rtol=method.rtol,
+        precompute_products=method.precompute_products)
+    KSVD.sparse_coding(noncudamethod, data, dictionary; DtD, DtY)
 end
+
+#function KSVD.sparse_coding2(method::CUDAAcceleratedMatchingPursuit,
+#    data::AbstractMatrix{T}, dictionary::AbstractMatrix{T}) where {T}
+#    K = size(dictionary, 2)
+#    N = size(data, 2)
+#
+#    DtD = dictionary' * dictionary
+#    Dt_dev = CuMatrix(dictionary')
+#
+#    op(Y_batch::CuMatrix) = Dt_dev * Y_batch
+#    if isnothing(method.workspace)
+#        method.workspace = make_batched_device_op_workspace(op, data)
+#    end
+#    # workspace = BatchedDeviceOpWorkspace(4096, 2*512, 2*512)
+#    # ch, task = batched_device_op(op, workspace, Y)
+#    ch, task = batched_device_op(op, method.workspace, data)
+#
+#    X_batches = map(ch) do (idx, DtY)
+#        idx => sparse_coding(ParallelMatchingPursuit(), @view(data[:, idx]), dictionary; DtY, DtD)
+#    end
+#    # X_batches = collect(ch)
+#    # @info size(X_batches), fetch(task)
+#    hcat(last.(sort(collect(X_batches); by=first))...)
+#end
 
 end # KSVDCudaExt
